1926fecd8a46a0e88149fff74316b2de98ecf428
Chad Cuddigan <cuddigan@gmail.com>
2023-05-18 10:26:44 -0700

    Giving the Lazy plugin manager a window border

--- nvim-mark2/lua/plugins.lua
+++ nvim-mark2/lua/plugins.lua
@@ -15,180 +15,182 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-    -- LSP Support
-    {
-        'neovim/nvim-lspconfig',
-        event = { "BufReadPre", "BufNewFile" },
-    }, -- Required
-    {
-        'williamboman/mason.nvim',
-        build = function()
-            pcall(vim.cmd, 'MasonUpdate')
-        end,
-    },
-    { 'williamboman/mason-lspconfig.nvim',         lazy = false }, -- For installing language servers
-    { 'WhoIsSethDaniel/mason-tool-installer.nvim', lazy = false }, -- For installing dap, linters, formatters, etc.
-
-    -- Autocompletion
-    {
-        'hrsh7th/nvim-cmp',
-        event = { "InsertEnter", "CmdlineEnter" },
-        dependencies = {
-            'hrsh7th/cmp-buffer',
-            'hrsh7th/cmp-path',
-            'hrsh7th/cmp-nvim-lua',
-            'hrsh7th/cmp-nvim-lsp',
-            'L3MON4D3/LuaSnip',
-            'saadparwaiz1/cmp_luasnip',
-        }
-    },
-    { 'rafamadriz/friendly-snippets',    lazy = false },
-    { 'onsails/lspkind.nvim',            lazy = false },
-    { "nvim-treesitter/nvim-treesitter", lazy = false, build = ":TSUpdate" },
-    { "nvim-tree/nvim-tree.lua",         lazy = false },
-    {
-        'nvim-telescope/telescope.nvim',
-        version = '*',
-        dependencies = {
-            'nvim-lua/plenary.nvim',
-            'nvim-telescope/telescope-ui-select.nvim',
-            'kdheepak/lazygit.nvim',
-            'voldikss/vim-floaterm',
-        }
-    },
-    {
-        'folke/which-key.nvim',
-        tag = 'v1.2.2',
-        lazy = false
-    },
-    { 'nvim-lualine/lualine.nvim',           event = "VeryLazy" },
-    { 'lukas-reineke/indent-blankline.nvim', lazy = false },
-    { 'numToStr/Comment.nvim',               lazy = false },
-    { 'ggandor/leap.nvim',                   lazy = false },
-    { "akinsho/bufferline.nvim",             dependencies = "nvim-tree/nvim-web-devicons", lazy = false },
-    { 'windwp/nvim-autopairs',               lazy = false },
-    { 'lewis6991/gitsigns.nvim',             event = "BufEnter" },
-    { 'David-Kunz/markid',                   lazy = false }, -- highlight same-name identifiers with the same colors
-    {
-        "folke/zen-mode.nvim",
-        cmd = "ZenMode",
-        config = function()
-            vim.api.nvim_set_hl(0, 'ZenBg', { ctermbg = 0 })
-        end
-    },
-    {
-        "nmac427/guess-indent.nvim",
-        config = function()
-            require("guess-indent").setup()
-        end
-    },
-    { "mg979/vim-visual-multi",      lazy = false }, -- better multi-line editing
-
-    --- Themes
-    { 'projekt0n/github-nvim-theme', tag = 'v0.0.7',      lazy = false },
-    { 'rose-pine/neovim',            name = 'rose-pine',  lazy = false },
-    { 'rebelot/kanagawa.nvim',       name = 'kanagawa',   lazy = false },
-    { 'catppuccin/nvim',             name = 'catppuccin', lazy = false },
-    { 'folke/tokyonight.nvim',       lazy = false },
-    { 'navarasu/onedark.nvim',       lazy = false },
-    { 'tiagovla/tokyodark.nvim',     lazy = false },
-
-    --
-    { 'folke/twilight.nvim',         lazy = false },
-    {
-        'folke/todo-comments.nvim',
-        lazy = false,
-        dependencies = 'nvim-lua/plenary.nvim'
-    },
-    { 'folke/zen-mode.nvim',                 lazy = false },
-    {
-        'ellisonleao/glow.nvim',
-        lazy = false,
-        cmd = "Glow",
-        opts = {
-            glow_path = "C:\\Users\\Rohit\\scoop\\shims\\glow.exe",
+        -- LSP Support
+        {
+            'neovim/nvim-lspconfig',
+            event = { "BufReadPre", "BufNewFile" },
+        }, -- Required
+        {
+            'williamboman/mason.nvim',
+            build = function()
+                pcall(vim.cmd, 'MasonUpdate')
+            end,
         },
-        enabled = false,
-    },
-    { 'mcauley-penney/tidy.nvim',            lazy = false },
-    { 'terrortylor/nvim-comment',            lazy = false },
-    { 'lukas-reineke/indent-blankline.nvim', lazy = false },
-    { 'akinsho/toggleterm.nvim',             lazy = false },
-    { 'j-hui/fidget.nvim',                   event = "LspAttach" },
-    { 'glepnir/dashboard-nvim',              lazy = false },
-    {
-        'rohit-kumar-j/cmake-tools.nvim',
-        dependencies = {
-            'nvim-tree/nvim-web-devicons',
-            'nvim-lua/plenary.nvim',
-            'nvim-telescope/telescope-ui-select.nvim',
-            'mfussenegger/nvim-dap',
-        },
-        lazy = false
-    },
+        { 'williamboman/mason-lspconfig.nvim',         lazy = false }, -- For installing language servers
+        { 'WhoIsSethDaniel/mason-tool-installer.nvim', lazy = false }, -- For installing dap, linters, formatters, etc.
 
-    -- Organization with `.norg` files
-    {
-        "nvim-neorg/neorg",
-        build = ":Neorg sync-parsers",
-        opts = {
-            load = {
-                ["core.defaults"] = {}, -- Loads default behaviour
-                ["core.norg.concealer"] = {
-                    config = {
-                        folds = true,
-                        icon_preset = 'varied', -- basic, diamond, varied
-                    },
-                },                              -- Adds pretty icons to your documents
-                -- ["core.norg.dirman"] = {      -- Manages Neorg workspaces
-                --     config = {
-                --         workspaces = {
-                --             notes = "~/notes",
-                --         },
-                --     },
-                -- },
+        -- Autocompletion
+        {
+            'hrsh7th/nvim-cmp',
+            event = { "InsertEnter", "CmdlineEnter" },
+            dependencies = {
+                'hrsh7th/cmp-buffer',
+                'hrsh7th/cmp-path',
+                'hrsh7th/cmp-nvim-lua',
+                'hrsh7th/cmp-nvim-lsp',
+                'L3MON4D3/LuaSnip',
+                'saadparwaiz1/cmp_luasnip',
+            }
+        },
+        { 'rafamadriz/friendly-snippets',    lazy = false },
+        { 'onsails/lspkind.nvim',            lazy = false },
+        { "nvim-treesitter/nvim-treesitter", lazy = false, build = ":TSUpdate" },
+        { "nvim-tree/nvim-tree.lua",         lazy = false },
+        {
+            'nvim-telescope/telescope.nvim',
+            version = '*',
+            dependencies = {
+                'nvim-lua/plenary.nvim',
+                'nvim-telescope/telescope-ui-select.nvim',
+                'kdheepak/lazygit.nvim',
+                'voldikss/vim-floaterm',
+            }
+        },
+        {
+            'folke/which-key.nvim',
+            tag = 'v1.2.2',
+            lazy = false
+        },
+        { 'nvim-lualine/lualine.nvim',           event = "VeryLazy" },
+        { 'lukas-reineke/indent-blankline.nvim', lazy = false },
+        { 'numToStr/Comment.nvim',               lazy = false },
+        { 'ggandor/leap.nvim',                   lazy = false },
+        { "akinsho/bufferline.nvim",             dependencies = "nvim-tree/nvim-web-devicons", lazy = false },
+        { 'windwp/nvim-autopairs',               lazy = false },
+        { 'lewis6991/gitsigns.nvim',             event = "BufEnter" },
+        { 'David-Kunz/markid',                   lazy = false }, -- highlight same-name identifiers with the same colors
+        {
+            "folke/zen-mode.nvim",
+            cmd = "ZenMode",
+            config = function()
+                vim.api.nvim_set_hl(0, 'ZenBg', { ctermbg = 0 })
+            end
+        },
+        {
+            "nmac427/guess-indent.nvim",
+            config = function()
+                require("guess-indent").setup()
+            end
+        },
+        { "mg979/vim-visual-multi",      lazy = false }, -- better multi-line editing
+
+        --- Themes
+        { 'projekt0n/github-nvim-theme', tag = 'v0.0.7',      lazy = false },
+        { 'rose-pine/neovim',            name = 'rose-pine',  lazy = false },
+        { 'rebelot/kanagawa.nvim',       name = 'kanagawa',   lazy = false },
+        { 'catppuccin/nvim',             name = 'catppuccin', lazy = false },
+        { 'folke/tokyonight.nvim',       lazy = false },
+        { 'navarasu/onedark.nvim',       lazy = false },
+        { 'tiagovla/tokyodark.nvim',     lazy = false },
+
+        --
+        { 'folke/twilight.nvim',         lazy = false },
+        {
+            'folke/todo-comments.nvim',
+            lazy = false,
+            dependencies = 'nvim-lua/plenary.nvim'
+        },
+        { 'folke/zen-mode.nvim',                 lazy = false },
+        {
+            'ellisonleao/glow.nvim',
+            lazy = false,
+            cmd = "Glow",
+            opts = {
+                glow_path = "C:\\Users\\Rohit\\scoop\\shims\\glow.exe",
             },
+            enabled = false,
         },
-        enabled = false,
-        dependencies = { { "nvim-lua/plenary.nvim" } },
-    },
+        { 'mcauley-penney/tidy.nvim',            lazy = false },
+        { 'terrortylor/nvim-comment',            lazy = false },
+        { 'lukas-reineke/indent-blankline.nvim', lazy = false },
+        { 'akinsho/toggleterm.nvim',             lazy = false },
+        { 'j-hui/fidget.nvim',                   event = "LspAttach" },
+        { 'glepnir/dashboard-nvim',              lazy = false },
+        {
+            'rohit-kumar-j/cmake-tools.nvim',
+            dependencies = {
+                'nvim-tree/nvim-web-devicons',
+                'nvim-lua/plenary.nvim',
+                'nvim-telescope/telescope-ui-select.nvim',
+                'mfussenegger/nvim-dap',
+            },
+            lazy = false
+        },
 
-    -- Go IDE! Also mixing in some signature helpers and linters
-    {
-        "fatih/vim-go",
-        build = ":GoInstallBinaries",
-        lazy  = false,
-    },
-    {
-        'ray-x/lsp_signature.nvim', -- function signatures as you type!
-        lazy = false,
-        -- configured in lspconfig.lua!
-    },
-    { 'jose-elias-alvarez/null-ls.nvim', lazy = false, },
-    {
-        'kosayoda/nvim-lightbulb', -- code actions lightbulb icon
-        lazy = false,
-        config = function()
-            require('nvim-lightbulb').setup({
-                autocmd = { enabled = true },
-            })
+        -- Organization with `.norg` files
+        {
+            "nvim-neorg/neorg",
+            build = ":Neorg sync-parsers",
+            opts = {
+                load = {
+                    ["core.defaults"] = {}, -- Loads default behaviour
+                    ["core.norg.concealer"] = {
+                        config = {
+                            folds = true,
+                            icon_preset = 'varied', -- basic, diamond, varied
+                        },
+                    },                              -- Adds pretty icons to your documents
+                    -- ["core.norg.dirman"] = {      -- Manages Neorg workspaces
+                    --     config = {
+                    --         workspaces = {
+                    --             notes = "~/notes",
+                    --         },
+                    --     },
+                    -- },
+                },
+            },
+            enabled = false,
+            dependencies = { { "nvim-lua/plenary.nvim" } },
+        },
 
-            -- better icon
-            vim.fn.sign_define("LightBulbSign", { text = "", texthl = "LspDiagnosticsDefaultInformation" })
-        end
-    },
-    {
-        'ahmedkhalf/project.nvim',
-        lazy = false,
-        config = function()
-            require("project_nvim").setup {}
-            require('telescope').load_extension('projects')
-        end
-    },
+        -- Go IDE! Also mixing in some signature helpers and linters
+        {
+            "fatih/vim-go",
+            build = ":GoInstallBinaries",
+            lazy  = false,
+        },
+        {
+            'ray-x/lsp_signature.nvim', -- function signatures as you type!
+            lazy = false,
+            -- configured in lspconfig.lua!
+        },
+        { 'jose-elias-alvarez/null-ls.nvim', lazy = false, },
+        {
+            'kosayoda/nvim-lightbulb', -- code actions lightbulb icon
+            lazy = false,
+            config = function()
+                require('nvim-lightbulb').setup({
+                    autocmd = { enabled = true },
+                })
 
-    -- Wiki!
-    {
-        'vimwiki/vimwiki',
-        lazy = false,
+                -- better icon
+                vim.fn.sign_define("LightBulbSign", { text = "", texthl = "LspDiagnosticsDefaultInformation" })
+            end
+        },
+        {
+            'ahmedkhalf/project.nvim',
+            lazy = false,
+            config = function()
+                require("project_nvim").setup {}
+                require('telescope').load_extension('projects')
+            end
+        },
+
+        -- Wiki!
+        {
+            'vimwiki/vimwiki',
+            lazy = false,
+        },
     },
-})
+    { ui = { border = 'rounded' } }
+)
